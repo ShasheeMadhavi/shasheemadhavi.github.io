acaefb610553835beb35895854ba7e1fd3ed510f
ShasheeMadhavi <92915@fhss.sjp.ac.lk>
2021-06-10 15:39:41 +0530

    Updates

---
+++ precache-manifest.b92edb6aaf189233020ea77eaf871b67.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3e2ed4f2779cc29e3e249a93b3615d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "376841213f39780c22a3",
+    "url": "/static/css/main.dd66ec12.chunk.css"
+  },
+  {
+    "revision": "9def30b24cfbb4d0f62d",
+    "url": "/static/js/2.1a7296ba.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.1a7296ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376841213f39780c22a3",
+    "url": "/static/js/main.37f35779.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "82cff7d69cb75d6a059cff39b741ebf7",
+    "url": "/static/media/SARA-SETH.82cff7d6.TTF"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "50afb4ef9f98b1effab0ea8a121a38ae",
+    "url": "/static/media/logo-white.50afb4ef.png"
+  },
+  {
+    "revision": "32d6ad73702f09f090e50c0ef5866e3a",
+    "url": "/static/media/madhavi.32d6ad73.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
